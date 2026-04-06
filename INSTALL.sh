5bbb14e1e6da5f975c48eca272e13fc102b4dc08
Miguel Eduardo <miguelodoutor@gmail.com>
2026-04-06 08:23:04 -0300

    bug fix: erro syntaxe

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 # COLORS
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-PURPLE='\033[38;5;135m'
-DEFAULT='\033[0m'
+G='\033[1;32m'      # GREEN
+R='\033[1;31m'      # RED
+B='\033[0;33m'      # BLUE
+P='\033[1;35m'      # PURPLE
+O='\033[1;93m'      # ORANGE
+D='\033[0m'         # DEFAULT
 
 # VALUES
 SCRIPT_NAME="nx_upkeep"
@@ -14,20 +15,20 @@ INSTALL_DIR="/usr/local/bin"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/nexus_command/${SCRIPT_NAME}"
 
 # start here
-echo -e "${RED}🛠️ NEXUS BOT:"
-echo -e "   ${BLUE}The global command '$SCRIPT_NAME' be created in '$SCRIPT_PATH'...${DEFAULT}"
+echo -e "${P}🛠️ NEXUS BOT:"
+echo -e "   ${B}The global command ${P}$SCRIPT_NAME${B} be created in '$SCRIPT_PATH'...${D}"
 
 # CHECK IF COMAND EXISTE IN THIS FOULDER
 if [[ ! -f "$SCRIPT_PATH" ]]; then
-    echo -e "${RED}🛠️ NEXUS BOT: You removed the script from this foulder, please return it...${DEFAULT}"
+    echo -e "${P}🛠️ NEXUS BOT: ${R}You removed the script from this foulder, please return it...${D}"
     exit 1
 fi
 
 # INSTAL THE SCRIPT
-sudo cp "$SCRIPT_PATH" "$INSTALL_DIR"
+sudo cp -uv "$SCRIPT_PATH" "$INSTALL_DIR"
 sudo chmod +x "$INSTALL_DIR/${SCRIPT_NAME}"
 
 # ENDING
-echo -e "${RED}🛠️ NEXUS BOT: ${GREEN}The ${PURPLE}NEXUS-COMAND${GREEN} has be instaled!!!"
+echo -e "${P}🛠️ NEXUS BOT: ${G}The ${P}NEXUS-COMAND${G} has be instaled!!!"
 echo ""
-echo -e "for execute it write ${RED}$SCRIPT_NAME${GREEN} in your console...${DEFAULT}"
+echo -e "for execute it write ${B}$SCRIPT_NAME${G} in your console...${D}"
